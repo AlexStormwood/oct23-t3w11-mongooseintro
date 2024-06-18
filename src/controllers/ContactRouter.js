f282c60883fda5f9acadfe8ff5906b632f44be31
Alex <alex.holder@bigfootds.com>
2024-06-18 20:45:04 +1000

    feat: CRUD skeleton, get all Contacts implemented

--- src/controllers/ContactRouter.js
+++ src/controllers/ContactRouter.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { Contact } = require("../models/ContactModel");
 const router = express.Router();
 
 router.get("/", (request, response) => {
@@ -7,6 +8,37 @@ router.get("/", (request, response) => {
 	});
 });
 
+
+// GET localhost:3000/contacts/all
+router.get("/all", async (request,response) => {
+	let results = await Contact.find().exec();
+	console.log("Found documents:");
+	console.log(results);
+	response.json({
+		message: "Found documents!",
+		data: results
+	});
+});
+
+// GET localhost:3000/contacts/1234
+router.get("/:id", (request, response) => {
+
+});
+
+// POST localhost:3000/contacts/
+router.post("/", (request, response) => {
+
+});
+
+// PATCH localhost:3000/contacts/1234
+router.patch("/:id", (request, response) => {
+
+});
+
+// DELETE localhost:3000/contacts/1234
+router.delete("/:id", (request, response) => {
+
+});
 
 
 module.exports = router;
